be1161f16aac0d350e1223b20ba6cdab883a5007
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2023-11-30 10:10:13 +0700

    draft open customize

--- src/components/Navbar/ModalOptionTools.jsx
+++ src/components/Navbar/ModalOptionTools.jsx
@@ -1,28 +1,28 @@
 import "./modalOptionTools.css";
 
-const ModalOptionTool = () => {
+const ModalOptionTool = ({ types }) => {
   return (
     <div className="modalOption">
       <div className="option">
-        <div className="option-text">
+        <div className="option-text" onClick={types(true)}>
           <h2>Text to speech</h2>
           <p>Transfer your text to audio </p>
         </div>
       </div>
       <div className="option">
-        <div className="option-text">
+        <div className="option-text" onClick={types(false)}>
           <h1>Speech to text</h1>
           <p>Convert your voice to text</p>
         </div>
       </div>
       <div className="option">
-        <div className="option-text">
+        <div className="option-text" onClick={types(false)}>
           <h1>Record to text</h1>
           <p>Take a direct audio recording and turn it into text</p>
         </div>
       </div>
       <div className="option">
-        <div className="option-text">
+        <div className="option-text" onClick={types(false)}>
           <h1>Image to text</h1>
           <p>Conver the image to text</p>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,10 +7,11 @@ import {
   TextT,
   VideoCamera,
 } from "@phosphor-icons/react/dist/ssr";
-// import { useState } from "react";
 import Popup from "reactjs-popup";
 import ModalOptionTool from "./ModalOptionTools";
 import "./navbar.css";
+import { useState } from "react";
+import Customize from "../Customize/Customize";
 const Navbar = (props) => {
   const {
     setIsOpenInputText,
@@ -20,52 +21,58 @@ const Navbar = (props) => {
     setIsOpenInputImage,
     setIsOpenInputRecor,
   } = props;
-  return (
-    <div className="container1">
-      <div className="navbar">
-        <div className="box_1" onClick={() => setIsOpenInputText(true)}>
-          <TextT size={32} />
-        </div>
-        <div className="box_1" onClick={() => setIsOpenInputImage(true)}>
-          <Image size={32} />
-        </div>
-        <div className="box_1" onClick={() => setIsOpenInputVideo(true)}>
-          <VideoCamera size={32} />
-        </div>
-        <div className="box_1" onClick={() => setIsOpenInputAudio(true)}>
-          <GooglePodcastsLogo size={32} />
-        </div>
-        <div className="box_1" onClick={() => setIsOpenInputRecor(true)}>
-          <Microphone size={32} />
-        </div>
-        <div className="box_1" onClick={() => setIsOpenInputURL(true)}>
-          <LinkSimple size={32} />
-        </div>
-        <div className="box_line"></div>
 
-        <div className="box_1">
-          <Popup
-            trigger={
-              <button>
-                <Hammer size={32} />
-              </button>
-            }
-            position="top center"
-            offsetY={25}
-            // disabled={true}
-            arrowStyle={{
-              color: "rgba(0, 0, 0, 0.15)",
-              width: "30px",
-              bottom: 0,
-              right: "-10px",
-              // border: "1px solid #CCCCCC"
-            }}
-          >
-            <ModalOptionTool />
-          </Popup>
+  const [type, setType] = useState(null);
+  return (
+    <>
+      <div className="container1">
+        <div className="navbar">
+          <div className="box_1" onClick={() => setIsOpenInputText(true)}>
+            <TextT size={32} />
+          </div>
+          <div className="box_1" onClick={() => setIsOpenInputImage(true)}>
+            <Image size={32} />
+          </div>
+          <div className="box_1" onClick={() => setIsOpenInputVideo(true)}>
+            <VideoCamera size={32} />
+          </div>
+          <div className="box_1" onClick={() => setIsOpenInputAudio(true)}>
+            <GooglePodcastsLogo size={32} />
+          </div>
+          <div className="box_1" onClick={() => setIsOpenInputRecor(true)}>
+            <Microphone size={32} />
+          </div>
+          <div className="box_1" onClick={() => setIsOpenInputURL(true)}>
+            <LinkSimple size={32} />
+          </div>
+          <div className="box_line"></div>
+
+          <div className="box_1">
+            <Popup
+              trigger={
+                <button>
+                  <Hammer size={32} />
+                </button>
+              }
+              position="top center"
+              offsetY={25}
+              // disabled={true}
+              arrowStyle={{
+                color: "rgba(0, 0, 0, 0.15)",
+                width: "30px",
+                bottom: 0,
+                right: "-10px",
+                // border: "1px solid #CCCCCC"
+              }}
+            >
+              <ModalOptionTool types={setType} />
+            </Popup>
+          </div>
         </div>
       </div>
-    </div>
+      {type ? <Customize toVoice={true} /> : <Customize />}
+      {/* {!type && <Customize toVoice={false} />} */}
+    </>
   );
 };
 
