f856122adb9f1f38d968b8ec66bd8faf321d0cca
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2023-11-30 16:08:01 +0700

    select tool to customize

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,104 +31,108 @@ const Navbar = (props) => {
     setIsOpenInputImage,
     setIsOpenInputRecor,
   } = props;
-
-  const [type, setType] = useState(null);
   const click = useSelector((state) => state.click.value);
   const file = useSelector((state) => state.file.value);
 
-  return (
-    <div className="container1">
-      <div className="navbar">
-        <div
-          className="box_1"
-          onClick={() => {
-            setIsOpenInputText(true);
-            addElement("text");
-          }}
-        >
-          <TextT size={32} />
-        </div>
-        <div
-          className="box_1"
-          onClick={() => {
-            setIsOpenInputImage(true);
-            addElement("img");
-          }}
-        >
-          <Image size={32} />
-        </div>
-        <div
-          className="box_1"
-          onClick={() => {
-            addElement("camera");
-            setIsOpenInputVideo(true);
-          }}
-        >
-          <VideoCamera size={32} />
-        </div>
-        <div
-          className="box_1"
-          onClick={() => {
-            addElement("audioUpload");
-            setIsOpenInputAudio(true);
-          }}
-        >
-          <GooglePodcastsLogo size={32} />
-        </div>
-        <div
-          className="box_1"
-          onClick={() => {
-            addElement("record");
-            setIsOpenInputRecor(true);
-          }}
-        >
-          <Microphone size={32} />
-        </div>
-        <div
-          className="box_1"
-          onClick={() => {
-            addElement("link");
-            setIsOpenInputURL(true);
-          }}
-        >
-          <LinkSimple size={32} />
-        </div>
-        <div className="box_line"></div>
+  const [type, setType] = useState(null);
 
-        <div className="box_1">
-          {(isOpenInputText && click) ||
-          (isOpenInputURL && click) ||
-          (isOpenInputAudio && file) ||
-          (isOpenInputVideo && file) ||
-          (isOpenInputImage && file) ||
-          (isOpenInputRecor && file) ? (
-            <Popup
-              trigger={
-                <button>
-                  <Hammer size={32} />
-                </button>
-              }
-              position="top center"
-              offsetY={25}
-              // disabled={true}
-              arrowStyle={{
-                color: "rgba(0, 0, 0, 0.15)",
-                width: "30px",
-                bottom: 0,
-                right: "-10px",
-                // border: "1px solid #CCCCCC"
-              }}
-            >
-              <ModalOptionTool />
-            </Popup>
-          ) : (
-            <div>
-              <ShieldSlash size={32} color="red" />
-            </div>
-          )}
+  return (
+    <>
+      <div className="container1">
+        <div className="navbar">
+          <div
+            className="box_1"
+            onClick={() => {
+              setIsOpenInputText(true);
+              addElement("text");
+            }}
+          >
+            <TextT size={32} />
+          </div>
+          <div
+            className="box_1"
+            onClick={() => {
+              setIsOpenInputImage(true);
+              addElement("img");
+            }}
+          >
+            <Image size={32} />
+          </div>
+          <div
+            className="box_1"
+            onClick={() => {
+              addElement("camera");
+              setIsOpenInputVideo(true);
+            }}
+          >
+            <VideoCamera size={32} />
+          </div>
+          <div
+            className="box_1"
+            onClick={() => {
+              addElement("audioUpload");
+              setIsOpenInputAudio(true);
+            }}
+          >
+            <GooglePodcastsLogo size={32} />
+          </div>
+          <div
+            className="box_1"
+            onClick={() => {
+              addElement("record");
+              setIsOpenInputRecor(true);
+            }}
+          >
+            <Microphone size={32} />
+          </div>
+          <div
+            className="box_1"
+            onClick={() => {
+              addElement("link");
+              setIsOpenInputURL(true);
+            }}
+          >
+            <LinkSimple size={32} />
+          </div>
+          <div className="box_line"></div>
+
+          <div className="box_1">
+            {(isOpenInputText && click) ||
+            (isOpenInputURL && click) ||
+            (isOpenInputAudio && file) ||
+            (isOpenInputVideo && file) ||
+            (isOpenInputImage && file) ||
+            (isOpenInputRecor && file) ? (
+              <Popup
+                trigger={
+                  <button>
+                    <Hammer size={32} />
+                  </button>
+                }
+                position="top center"
+                offsetY={25}
+                // disabled={true}
+                arrowStyle={{
+                  color: "rgba(0, 0, 0, 0.15)",
+                  width: "30px",
+                  bottom: 0,
+                  right: "-10px",
+                  // border: "1px solid #CCCCCC"
+                }}
+              >
+                <ModalOptionTool types={setType} />
+              </Popup>
+            ) : (
+              <div>
+                <ShieldSlash size={32} color="red" />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      {type === "ToVoice" && <Customize toVoice={true} />}
+      {type === "ToText" && <Customize />}
+    </>
   );
 };
 
