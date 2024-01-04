189adfb6b405c1f6d5ab6158527032db28c81132
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2024-01-04 15:07:11 +0700

    respon customize

--- src/components/DropdownList/DropdownList.jsx
+++ src/components/DropdownList/DropdownList.jsx
@@ -3,14 +3,17 @@ import React, { useState, useRef, useEffect } from "react";
 function Dropdownlist({ title, options, callback, selected }) {
   return (
     <div className="flex flex-col mx-1 my-2 w-full">
-      <p className="font-bold">{title}</p>
+      <p className="font-bold md:text-[18px] lg:text-[18px] text-[14px]">
+        {title}
+      </p>
       {
         <select
           onChange={(e) => callback(e.target.value)}
-          className="w-full border-gray-300 rounded-md h-full"
-          value={selected}>
+          className="w-full border-gray-300 rounded-md h-full md:text-lg lg:text-lg text-[14px] lg:p-2 md:p-2 p-1"
+          value={selected}
+        >
           {options.map((o, i) => (
-            <option value={i} key={i} className="text-black p-2 text-lg">
+            <option value={i} key={i} className="text-black p-2 ">
               {o}
             </option>
           ))}
